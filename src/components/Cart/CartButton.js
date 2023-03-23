09608a86d3f300a980bbc18aecfa200bb36fd641
PoeticCrane <poeticcrane@gmail.com>
2023-03-23 15:39:30 +0300

    add Cart Visibility

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,16 @@
 import styles from './CartButton.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { mainActions } from '../../store/main-slice';
 const CartButton = () => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(mainActions.toggleCartVisibility());
+  };
   const totalQantity = useSelector((state) => {
-    
     return state.products.base.item1.quantity;
   });
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={toggleCartHandler}>
       <span>Корзина</span>
       <span className={styles.badge}>{totalQantity}</span>
     </button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
-import redux from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './base-products';
-
+import mainReducer from './main-slice';
 const store = configureStore({
   reducer: {
     products: productsReducer,
+    main: mainReducer,
   },
 });
 export default store;
